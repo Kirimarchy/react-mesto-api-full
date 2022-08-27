c59216b12a59a0da3c37583279831c048f85c8da
Marianna Kaledina <kamariann@yandex.ru>
2022-08-27 19:30:42 +0300

    crash

--- backend/app.js
+++ backend/app.js
@@ -30,12 +30,6 @@ app.post('/signin', celebrate({
   }),
 }), login);
 
-app.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
-
 app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
