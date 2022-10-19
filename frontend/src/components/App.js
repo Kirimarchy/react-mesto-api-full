5240573436de5454857d8bd9747c20a5765e4539
Marianna Kaledina <kamariann@yandex.ru>
2022-10-19 21:22:36 +0300

    updateCards

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -125,8 +125,8 @@ const App = () => {
                 setCurrentUser(res);
                 setCurrentUserInfo(res);
                 navigate('/');
-            });
-
+            })
+            .then(updateCards());
         }
     }
 
